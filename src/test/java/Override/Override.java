3907e1d850a003e8d80ef318e7554fdd1b1daa48
AnryAnryK <andkay@yandex.ru>
2024-12-03 16:05:26 +0300

    2024.12.03 -  добавил пакет с Override (в рамках изучения Java Core)

---
+++ src/test/java/Override/Override.java
@@ -0,0 +1,25 @@
+package Override;
+
+public class Override {
+    public static void main(String[] args) {
+Parent parent = new Parent();
+Child child = new Child();
+        System.out.println(parent.method());
+        System.out.println(child.method());
+    }
+}
+
+class Parent {
+    String method() {
+        return "Родительский метод";
+    }
+}
+
+
+class Child extends Parent {
+    @java.lang.Override
+    String method() {
+        return "Переопределенный Родительский метод в Дочерний класс";
+    }
+}
+
