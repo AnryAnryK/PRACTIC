f06f11aed07b7f19f33642b296afb1cfc85c7c15
AnryAnryK <andkay@yandex.ru>
2024-12-06 12:51:20 +0300

    2024.12.06 -  c "while (true);  // с этим блоком Программа будет Работать вечно )))" _ в Пакет "ErrorsAndExceptions" (Спискок вопросов по Джава)

--- src/test/java/ErrorsAndExceptions/ErrorsAndExceptions.java
+++ src/test/java/ErrorsAndExceptions/ErrorsAndExceptions.java
@@ -5,19 +5,17 @@ public class ErrorsAndExceptions {
 	public static void main(String[] args) {
 		ErrorsAndExceptions errorsAndExceptions = new ErrorsAndExceptions();
 		errorsAndExceptions.testPush();
-
-//			System.out.println(1/0);
 	}
 
 	public Check check;  // а с синтаксисом = new Check()  - объект check будет создан и поэтому NullPointerException  не вылезет !
 
 	public void testPush() {
-		try {           //с этим блоком _ try { } catch (Exception e) {}  _  ошибка NullPointerException  выпадать не будет, т.к. мы вручную говорим среде, чтобы этот случай попадал в Исключения (и, соответственно, обрабатывался БЕЗ ошибки !)
+		try {           // с этим блоком _ try { } catch (Exception e) {}  _  ошибка NullPointerException  выпадать не будет, т.к. мы вручную говорим среде, чтобы этот случай попадал в Исключения (и, соответственно, обрабатывался БЕЗ ошибки !)
 			check.push2();
 		} catch (Exception e) {
-		}
-		finally {
+		} finally {
 			System.out.println(check);
+//		while (true);  // с этим блоком Программа будет Работать вечно )))
 		}
 	}
 }
